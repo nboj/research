a5e99c86cb3b1fecb4d57ea230dee6d77d8f252c
nboj <60493251+nboj@users.noreply.github.com>
2025-06-16 15:16:31 -0400

    added amplify auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+'use client'
+import { signIn } from "aws-amplify/auth";
 import Image from "next/image";
 
 export default function Home() {
+  const test = async () => {
+    const res = await signIn({
+      username: "bob",
+      password: "builder"
+    });
+    console.log(res);
+  }
   return (
     <div>
+      <button onClick={test}>test</button>
     </div>
   );
 }
