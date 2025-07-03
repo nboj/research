07ebbe5ede4f85ed20c76f36c8f85cd7a2f2ff93
Nboj <60493251+nboj@users.noreply.github.com>
2025-07-03 14:23:14 -0400

    updated urls

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,16 +5,18 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
-        loginWith: {
-                email: true,
-                externalProviders: {
-                        callbackUrls: [
-                                'http://localhost:3000/api/auth/sign-in-callback',
-                        ],
-                        logoutUrls: [
-                                'http://localhost:3000/api/auth/sign-out-callback',
-                        ],
-                },
+    loginWith: {
+        email: true,
+        externalProviders: {
+            callbackUrls: [
+                'http://localhost:3000/api/auth/sign-in-callback',
+                'https://main.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-in-callback',
+            ],
+            logoutUrls: [
+                'http://localhost:3000/api/auth/sign-out-callback',
+                'https://main.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-out-callback',
+            ],
         },
+    },
 
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
 'use client'
 
-export default function Home() {
+import { Textarea } from "@heroui/react";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import { useCallback, useState } from "react";
+
+type Prompt = {
+    raw: string;
+    img?: {
+        src: string;
+    }
+}
+
+type PromptSelectionProps = Readonly<{
+    disabled: boolean;
+    onChange: (newPrompt: Prompt) => void;
+}>
+const PromptSection = ({ disabled, onChange }: PromptSelectionProps) => {
+    const [prompt, setPrompt] = useState<Prompt>({ raw: "" });
+    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange({ ...prompt, raw: e.target.value });
+        setPrompt((current: Prompt) => {
+            return { ...current, raw: e.target.value };
+        });
+    }, [setPrompt, prompt]);
     return (
         <div>
+            <Textarea label="Prompt" value={prompt.raw} placeholder="Enter your prompt..." onChange={handleChange} />
+            <div>
+                {
+                    prompt.img && (
+                        <Image src={prompt.img.src} width={100} height={100} alt={"Generated image."} />
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default function Home() {
+    const [prompt1, setPrompt1] = useState<Prompt>({ raw: "" });
+    const [prompt2, setPrompt2] = useState<Prompt>({ raw: "" });
+    return (
+        <div>
+            <PromptSection disabled onChange={(newPrompt: Prompt) => setPrompt1(newPrompt)} />
             <a href="/api/auth/sign-out">Sign Out</a>
         </div>
     );
