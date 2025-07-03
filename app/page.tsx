734b2ecbdcc6dbf0723834fc29c2681f6b6c338e
Nboj <60493251+nboj@users.noreply.github.com>
2025-07-03 13:44:29 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 export default function Home() {
-        return (
-                <div>
-                        <a href="/api/auth/sign-out">Sign Out</a>
-                </div>
-        );
+    return (
+        <div>
+            <a href="/api/auth/sign-out">Sign Out</a>
+        </div>
+    );
 }
