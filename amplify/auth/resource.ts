e5155d8514d4371c749e5b6570fc189f2a2973e4
nboj <60493251+nboj@users.noreply.github.com>
2025-07-04 10:13:59 -0400

    fixed build errors

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -5,20 +5,20 @@ import { defineAuth } from '@aws-amplify/backend';
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
-    loginWith: {
-        email: true,
-        externalProviders: {
-            callbackUrls: [
-                'http://localhost:3000/api/auth/sign-in-callback',
-                'https://main.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-in-callback',
-                "https://development.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-in-callback"
-            ],
-            logoutUrls: [
-                'http://localhost:3000/api/auth/sign-out-callback',
-                'https://main.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-out-callback',
-                "https://development.d1mrgpre7451wb.amplifyapp.com/auth/sign-out-callback"
-            ],
+        loginWith: {
+                email: true,
+                externalProviders: {
+                        callbackUrls: [
+                                'http://localhost:3000/api/auth/sign-in-callback',
+                                'https://main.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-in-callback',
+                                "https://development.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-in-callback"
+                        ],
+                        logoutUrls: [
+                                'http://localhost:3000/api/auth/sign-out-callback',
+                                'https://main.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-out-callback',
+                                "https://development.d1mrgpre7451wb.amplifyapp.com/api/auth/sign-out-callback"
+                        ],
+                },
         },
-    },
 
 });
