32faf1fe373f407fef5738f3162a1f28967702b2
nboj <60493251+nboj@users.noreply.github.com>
2025-10-05 16:18:08 -0400

    u

--- app/_lib/s3.ts
+++ app/_lib/s3.ts
@@ -1,10 +1,8 @@
 // lib/s3.ts
 import { S3Client } from "@aws-sdk/client-s3";
+import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
 
 export const s3 = new S3Client({
   region: process.env.AWS_REGION!,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_ACCESS_SECRET!,
-  },
+  credentials: fromNodeProviderChain(),
 });
